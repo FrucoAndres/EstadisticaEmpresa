3057282b7d85054c6c953c38e556bc4ce8aff519
Andres Lopez <salsitarpremorg121@gmail.com>
2024-04-01 18:26:05 -0500

    pequeños cambios en admin.py

--- inventario/admin.py
+++ inventario/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from inventario.models import VentaCamiseta
 
 class InventarioAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["id_camiseta","marca","talla_camiseta","cantidad_camiseta","precio_camiseta","nombre_cliente","metodo_de_pago", "fecha_de_pago", "pais", "ciudad", "barrio"]
 
-admin.site.register(VentaCamiseta)
+admin.site.register(VentaCamiseta, InventarioAdmin)
 # Register your models here.
